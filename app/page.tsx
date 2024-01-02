ab0383db4abe6b9c04b7ae01a352f9837af83713
“malcolmmutare” <mutarem@gmail.com>
2024-01-02 14:18:35 +0200

    array error fix

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default  function Home() {
     getCars();
   }, [fuel, year, limit, manufacturer, model])
 
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length <1 || !allCars;
+  // const isDataEmpty = !Array.isArray(allCars) || allCars.length <1 || !allCars;
 
 
 
